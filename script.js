29e9bd1195808d5b737f016ed83d1a4e991748d3
LINTANGDIKA <lintangdik0@gmail.com>
2021-11-27 23:47:49 +0700

    responsive halaman utama

--- script.js
+++ script.js
@@ -4,7 +4,4 @@ let isi = document.getElementsByClassName('list')[0];
 console.log(item1)
 item.addEventListener('click', function () {
     isi.classList.toggle('slide');
-    burger1[0].classList.toggle('putar1');
-    burger1[1].classList.toggle('putar2');
-    burger1[2].classList.toggle('putar3');
 });
